18011d94c5fc7e360aaf8495f4883525fed942f4
Ivan <iivvaann.andreev@gmail.com>
2018-11-17 18:47:47 +0000

    Javadoc for History class + formatting.

--- src/History.java
+++ src/History.java
@@ -2,19 +2,36 @@ import java.util.ArrayList;
 
 /**
  * Models a History class that contains entries with information about events.
+ *
  * @author Ivan Andreev
  */
 public class History {
+    /**
+     * Holds all the different entries.
+     */
     private ArrayList<HistoryEntry> history;
 
+    /**
+     * Creates an empty history object.
+     */
     public History() {
         history = new ArrayList<>();
     }
 
+    /**
+     * Get the full history.
+     *
+     * @return history A list of entries in the history.
+     */
     public ArrayList<HistoryEntry> getHistory() {
         return history;
     }
 
+    /**
+     * Add an entry to the history.
+     *
+     * @param historyEntry The entry to be added.
+     */
     public void addEntry(HistoryEntry historyEntry) {
         this.history.add(historyEntry);
     }
