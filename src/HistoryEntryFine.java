941906b1093a61701f06a440848774b1fed6e908
Ivan <iivvaann.andreev@gmail.com>
2018-11-17 18:27:19 +0000

    Javadoc comments. Formatted the whole file.

--- src/HistoryEntryFine.java
+++ src/HistoryEntryFine.java
@@ -15,14 +15,25 @@ public class HistoryEntryFine extends HistoryEntryMoneyTransaction {
      */
     private int daysOverdue;
 
-
+    /**
+     * Creates a fine type of history entry.
+     *
+     * @param date        The date of the fine.
+     * @param amount      The amount due.
+     * @param daysOverdue The number of days the item is overdue.
+     * @param item        The item that was not returned on time.
+     */
     public HistoryEntryFine(Date date, double amount, int daysOverdue, String item) {
         super(date, amount);
         this.daysOverdue = daysOverdue;
         this.item = item;
     }
 
-
+    /**
+     * Get the number of days an item is overdue.
+     *
+     * @return daysOverdue The number of days.
+     */
     public int getDaysOverdue() {
         return daysOverdue;
     }
