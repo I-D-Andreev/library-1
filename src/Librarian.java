320cda7133828632c2eab2093bea19b2f2e94900
Sian Pike <sian.pike@icloud.com>
2018-11-17 11:13:17 +0000

    I fucked up with merging conflicts but all is well now, everything should be fine.

--- src/Librarian.java
+++ src/Librarian.java
@@ -5,7 +5,7 @@
  */
 public class Librarian extends User {
 	private String staffNumber;
-	
+
 	/**
 	 * Creates a librarian account with the inputted information.
 	 * @param staff The librarians staff number.
@@ -15,101 +15,42 @@ public class Librarian extends User {
 	 * @param phone The librarians phone number.
 	 * @param image The librarians profile image.
 	 * @param ad The librarians home address.
-	 */		
+	 */
 	public Librarian(String staff, String first, String last, String user, String phone, String image, String ad) {
-		//super(first,last,user,phone,image,ad);
+		super(first,last,user,phone,image,ad);
 		this.setStaffNumber(staff);
 	}
-	
+
 	/**
 	 * Gets staff number of the librarian.
 	 * @return staffNumber The librarians staff number.
-	 */		
+	 */
 	public String getStaffNumber() {
 		return this.staffNumber;
 	}
-	
+
 	/**
 	 * Sets the staff number of the librarian.
 	 * @return staffNumber The librarian's new staff number.
-	 */		
+	 */
 	private void setStaffNumber(String staffNumber) {
 		this.staffNumber = staffNumber;
 	}
-	
+
 	/**
 	 * Checks whether librarian has admin access.
 	 * @return True as librarians always have admin access.
-	 */		
+	 */
 	public boolean hasAdminAccess() {
 		return true;
 	}
-	
+
 	/**
 	 * Gets the employment date of the librarian.
 	 * @return Librarian's employment date.
-	 */	
-	//needs to be completed, will be date class 
+	 */
+	//needs to be completed, will be date class
 	public String getEmploymentDate() {
 		return "date";
 	}
-=======
- *
- * @author Christina Meggs, Ivan Andreev
- */
-public class Librarian extends User {
-    private String staffNumber;
-
-    /**
-     * Creates a librarian account with the inputted information.
-     *
-     * @param staff The librarians staff number.
-     * @param first The librarians first name.
-     * @param last  The librarians last name.
-     * @param user  The librarians account username.
-     * @param phone The librarians phone number.
-     * @param image The librarians profile image.
-     * @param ad    The librarians home address.
-     */
-    public Librarian(String staff, String first, String last, String user, String phone, String image, String ad) {
-        super(first, last, user, phone, image, ad);
-        this.setStaffNumber(staff);
-    }
-
-    /**
-     * Gets staff number of the librarian.
-     *
-     * @return staffNumber The librarians staff number.
-     */
-    public String getStaffNumber() {
-        return this.staffNumber;
-    }
-
-    /**
-     * Sets the staff number of the librarian.
-     *
-     */
-    private void setStaffNumber(String staffNumber) {
-        this.staffNumber = staffNumber;
-    }
-
-    /**
-     * Checks whether librarian has admin access.
-     *
-     * @return True as librarians always have admin access.
-     */
-    public boolean hasAdminAccess() {
-        return true;
-    }
-
-    /**
-     * Gets the employment date of the librarian.
-     *
-     * @return Librarian's employment date.
-     */
-    //needs to be completed, will be date class
-    public String getEmploymentDate() {
-        return "date";
-    }
->>>>>>> ea95d1945fafc8b632bd78f7bf1cfe6b3d9741d2
 }
--- src/User.java
+++ src/User.java
@@ -5,316 +5,159 @@
  * @author Christina Meggs, Ivan Andreev
  */
 public abstract class User {
-    private final String username;
+	private final String username;
 
-    private String id;
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String profileImagePath; //set as String for now until class been created
-    private String address; //set as String for now until Address class been created
-
-    /**
-     * Creates a user profile with the basic information within all types of users.
-     *
-     * @param firstName The user's first name.
-     * @param lastName  The user's last name.
-     * @param user      The user's account username.
-     * @param phone     The user's phone number.
-     * @param imagePath The user's profile image.
-     * @param address   The user's home address.
-     */
-    public User(String firstName, String lastName, String user, String phone, String imagePath, String address) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = user;
-        this.phoneNumber = phone;
-        this.profileImagePath = imagePath;
-        this.address = address;
-    }
-
-    /**
-     * Gets the user's first name.
-     *
-     * @return firstName User's first name.
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * Sets the user's first name.
-     *
-     * @param firstName The user's new first name.
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * Gets user's last name.
-     *
-     * @return lastName User's last name.
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * Sets user's last name.
-     *
-     * @param lastName The user's new last name.
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * Gets the user's account username.
-     *
-     * @return username The user's account username.
-     */
-    public String getUsername() {
-        return username;
-    }
-
-
-    /**
-     * Gets the user's phone number.
-     *
-     * @return phoneNumber The user's phone number.
-     */
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * Sets the user's phone number.
-     *
-     * @param phoneNumber The user's new phone number.
-     */
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * Finds out if a phone number is valid.
-     *
-     * @return valid True if valid, false otherwise.
-     */
-    private boolean checkValidPhoneNumber() {
-        return true;
-    }
-
-    /**
-     * Gets the user's address.
-     *
-     * @return address The user's address.
-     */
-    //Instead of string will be address type when class is created
-    public String getAddress() {
-        return address;
-    }
-
-    /**
-     * Sets the user's address.
-     *
-     * @param address The new address of the user.
-     */
-    //Instead of string will be address type when class is created
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    /**
-     * Gets the path to the user's profile image.
-     *
-     * @return profileImage The path to the user's profile image.
-     */
-    //Instead of string will be profile image type when class is created
-    public String getProfileImagePath() {
-        return profileImagePath;
-    }
-
-    /**
-     * Sets the user's new profile image path.
-     *
-     * @param imagePath The user's new profile image path.
-     */
-    //Instead of string will be profile image type when class is created
-    public void setProfileImagePath(String imagePath) {
-        this.profileImagePath = imagePath;
-    }
-
-    /**
-     * Gets the date when the account was created.
-     *
-     * @return The creation date of the account.
-     */
-    public String getAccountCreationDate() {
-        //will do a date type of the current days date
-        return "16-11-2018";
-    }
-
-    /**
-     * Says whether the user has admin access.
-     */
-    abstract boolean hasAdminAccess();
-=======
- * This is an abstract class used as the base to create all types of users
- * @author Christina Meggs
- * @version 0.1
- */
-public abstract class User {
-	private String id; 
+	private String id;
 	private String firstName;
 	private String lastName;
-	private String username;
 	private String phoneNumber;
-	private String profileImage; //set as String for now until class been created 
-	private String address; //set as String for now until Address class been created 
-	
+	private String profileImagePath; //set as String for now until class been created
+	private String address; //set as String for now until Address class been created
+
 	/**
-	 * Creates a user profile with the basic information within all types of users
-	 * @param first The users first name.
-	 * @param last The users last name.
-	 * @param user The users account username.
-	 * @param phone The users phone number.
-	 * @param image The users profile image.
-	 * @param ad The users home address.
-	 */	
-	public User(String first, String last, String user, String phone, String image, String ad) {
-		this.firstName = first;
-		this.lastName = last;
+	 * Creates a user profile with the basic information within all types of users.
+	 *
+	 * @param firstName The user's first name.
+	 * @param lastName  The user's last name.
+	 * @param user      The user's account username.
+	 * @param phone     The user's phone number.
+	 * @param imagePath The user's profile image.
+	 * @param address   The user's home address.
+	 */
+	public User(String firstName, String lastName, String user, String phone, String imagePath, String address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.username = user;
 		this.phoneNumber = phone;
-		this.profileImage = image;
-		this.address = ad;
+		this.profileImagePath = imagePath;
+		this.address = address;
 	}
-	
+
 	/**
-	 * Gets user first name.
+	 * Gets the user's first name.
+	 *
 	 * @return firstName User's first name.
-	 */	
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	/**
-	 * Sets user first name.
-	 * @param first The users new first name.
-	 */		
-	public void setFirstName(String first) {
-		this.firstName = first;
+	 * Sets the user's first name.
+	 *
+	 * @param firstName The user's new first name.
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-	
+
 	/**
-	 * Gets user last name.
+	 * Gets user's last name.
+	 *
 	 * @return lastName User's last name.
-	 */		
+	 */
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	/**
-	 * Sets user last name.
-	 * @param last The users new last name.
-	 */		
-	public void setLastName(String last) {
-		this.lastName = last;
-	}	
-	
+	 * Sets user's last name.
+	 *
+	 * @param lastName The user's new last name.
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	/**
-	 * Gets the users account username.
-	 * @return username Username of users account.
-	 */		
+	 * Gets the user's account username.
+	 *
+	 * @return username The user's account username.
+	 */
 	public String getUsername() {
 		return username;
 	}
-	
+
+
 	/**
-	 * Sets the users account username.
-	 * @param user The new username for the account.
-	 */		
-	public void setUsername(String user) {
-		this.username = user;
-	}
-	
-	/**
-	 * Gets the users phone number.
-	 * @return phoneNumber the users phone number.
-	 */		
+	 * Gets the user's phone number.
+	 *
+	 * @return phoneNumber The user's phone number.
+	 */
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	
+
 	/**
-	 * Sets the users phone number.
-	 * @param phone The users new phone number.
-	 */		
-	public void setPhoneNumber(String phone) {
-		this.phoneNumber = phone;
+	 * Sets the user's phone number.
+	 *
+	 * @param phoneNumber The user's new phone number.
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
-	
+
 	/**
-	 * Finds out if phone number is valid.
-	 * @return valid Whether the phone number is valid or not.
-	 */		
-	//Is in the UML but no class for phone number, not sure how to check it
+	 * Finds out if a phone number is valid.
+	 *
+	 * @return valid True if valid, false otherwise.
+	 */
 	private boolean checkValidPhoneNumber() {
-		valid == true;
-		return valid; 
+		return true;
 	}
-	
+
 	/**
-	 * Gets the users address.
-	 * @return address The users address.
-	 */		
-	//Instead of string will be address type when class is created 
+	 * Gets the user's address.
+	 *
+	 * @return address The user's address.
+	 */
+	//Instead of string will be address type when class is created
 	public String getAddress() {
 		return address;
 	}
 
 	/**
-	 * Sets the users address.
-	 * @param ad The new address of the user.
-	 */		
-	//Instead of string will be address type when class is created	
-	public void setAddress(String ad) {
-		this.address = ad;
+	 * Sets the user's address.
+	 *
+	 * @param address The new address of the user.
+	 */
+	//Instead of string will be address type when class is created
+	public void setAddress(String address) {
+		this.address = address;
 	}
-	
-	/**
-	 * Gets the users profile image.
-	 * @return profileImage The users profile image.
-	 */		
-	//Instead of string will be profile image type when class is created 	
-	public String getProfileImage() {
-		return profileImage;
-	}
-	
-	/**
-	 * Sets the users profile image.
-	 * @param image The users new profile image.
-	 */		
-	//Instead of string will be profile image type when class is created		
-	public void setProfileImage(String image) {
-		this.profileImage = image;
-	}	
 
 	/**
-	 * Gets the date of when the account was created.
-	 * @return The creation date of account.
-	 */		
+	 * Gets the path to the user's profile image.
+	 *
+	 * @return profileImage The path to the user's profile image.
+	 */
+	//Instead of string will be profile image type when class is created
+	public String getProfileImagePath() {
+		return profileImagePath;
+	}
+
+	/**
+	 * Sets the user's new profile image path.
+	 *
+	 * @param imagePath The user's new profile image path.
+	 */
+	//Instead of string will be profile image type when class is created
+	public void setProfileImagePath(String imagePath) {
+		this.profileImagePath = imagePath;
+	}
+
+	/**
+	 * Gets the date when the account was created.
+	 *
+	 * @return The creation date of the account.
+	 */
 	public String getAccountCreationDate() {
 		//will do a date type of the current days date
 		return "16-11-2018";
 	}
-	
+
 	/**
-	 * Retrieves if the user has admin access.
-	 * Abstract so will be overridden.
-	 */		
+	 * Says whether the user has admin access.
+	 */
 	abstract boolean hasAdminAccess();
->>>>>>> 0d1b7f407c62fcc32833c4d96148cc36dc81d5ec
 }
