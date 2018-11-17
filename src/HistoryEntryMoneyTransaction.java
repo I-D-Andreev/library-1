aa9bd37417a199a42441f83d4ae17248aaeb09e6
Ivan <iivvaann.andreev@gmail.com>
2018-11-17 18:14:53 +0000

    Added javadoc and formatted the file.

--- src/HistoryEntryMoneyTransaction.java
+++ src/HistoryEntryMoneyTransaction.java
@@ -1,14 +1,30 @@
 import java.util.Date;
 
+/**
+ * Models a money transaction type of history entry.
+ */
 public class HistoryEntryMoneyTransaction extends HistoryEntry {
-
+    /**
+     * The amount of money in the transaction.
+     */
     private double amount;
 
-    public HistoryEntryMoneyTransaction(Date date, Double amount){
+    /**
+     * Creates a money transaction type of history entry.
+     *
+     * @param date   The date of the money transaction.
+     * @param amount The amount of the transaction.
+     */
+    public HistoryEntryMoneyTransaction(Date date, Double amount) {
         super(date);
         this.amount = amount;
     }
 
+    /**
+     * Get the amount of the transaction.
+     *
+     * @return amount The amount of the transaction.
+     */
     public double getAmount() {
         return amount;
     }
