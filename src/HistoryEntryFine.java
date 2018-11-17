e5440255fd9b1d8cbc18570ae316529a625d19db
Ivan <iivvaann.andreev@gmail.com>
2018-11-17 18:25:02 +0000

    HistoryEntryFine class

--- src/HistoryEntryFine.java
+++ src/HistoryEntryFine.java
@@ -4,20 +4,30 @@ import java.util.Date;
  * Models a fine type of history entry.
  */
 public class HistoryEntryFine extends HistoryEntryMoneyTransaction {
-    private int daysOverdue;
-    private String copy; // should be Copy type
 
-    public HistoryEntryFine(Date date, double amount, int daysOverdue, String copy) {
+    /**
+     * The item that is overdue and caused the fine.
+     */
+    private String item; // should be Copy type
+
+    /**
+     * The number of days the item was overdue.
+     */
+    private int daysOverdue;
+
+
+    public HistoryEntryFine(Date date, double amount, int daysOverdue, String item) {
         super(date, amount);
         this.daysOverdue = daysOverdue;
-        this.copy = copy;
+        this.item = item;
     }
+
 
     public int getDaysOverdue() {
         return daysOverdue;
     }
 
 //    public Copy getCopy() {
-//        return copy;
+//        return item;
 //    }
 }
