62366e3137902349237e7318a9afeecd3f137ddf
Sian Pike <sian.pike@icloud.com>
2018-11-18 11:52:39 +0000

    Added Profile Image Controller and FXML file.

---
+++ src/ProfileImageController.java
@@ -0,0 +1,41 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Controller class for the Profile Image UI.  Handles interactions from the user.
+ *
+ * @author Sian Pike
+ */
+public class ProfileImageController {
+
+    @FXML // fx:id="imageOneButton"
+    private Button imageOneButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageTwoButton"
+    private Button imageTwoButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageThreeButton"
+    private Button imageThreeButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageFourButton"
+    private Button imageFourButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageFiveButton"
+    private Button imageFiveButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageSixButton"
+    private Button imageSixButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageSevenButton"
+    private Button imageSevenButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageEightButton"
+    private Button imageEightButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="createAvatarButton"
+    private Button createAvatarButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="selectButton"
+    private Button selectButton; // Value injected by FXMLLoader
+
+}
