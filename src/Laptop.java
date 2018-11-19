80d3eef15fff86af6edd384c2237a46fdd47d22f
Steven <965909@swansea.ac.uk>
2018-11-19 01:36:01 +0000

    Added setters and getters for the laptop class

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,14 +1,25 @@
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class Laptop.
  * 
  * @author Steven Lekwowicz
  */
 public class Laptop extends Resource{
+	
+	/** The manufacturer. */
 	private String manufacturer;
+	
+	/** The model. */
 	private String model;
+	
+	/** The installed OS. */
 	private String installedOS;
+	
+	/** The late return fine per day. */
 	private double lateReturnFinePerDay;
+	
+	/** The max fine amount. */
 	private double maxFineAmount;
 	
 	/**
@@ -30,4 +41,78 @@ public class Laptop extends Resource{
 		this.lateReturnFinePerDay = 10.00;
 		this.maxFineAmount = 100.00;
 	}
+	
+	/**
+	 * Sets the laptop's new manufacturer.
+	 *
+	 * @param manufacturer The new manufacturer
+	 */
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	
+	/**
+	 * Gets the laptop's manufacturer.
+	 *
+	 * @return manufacturer The laptop's manufacturer
+	 */
+	public String getManufacturer() {
+		return this.manufacturer;
+	}
+	
+	/**
+	 * Sets the laptop's model.
+	 *
+	 * @param model The laptop's new model
+	 */
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	/**
+	 * Gets the laptop's model.
+	 *
+	 * @return model The laptop's model
+	 */
+	public String getModel() {
+		return this.model;
+	}
+	
+	/**
+	 * Sets the laptop's installed OS.
+	 *
+	 * @param installedOS The new OS installed on the laptop
+	 */
+	public void setInstalledOS(String installedOS) {
+		this.installedOS = installedOS;
+	}
+	
+	/**
+	 * Gets the laptop's installed OS.
+	 *
+	 * @return installedOS The OS installed on the laptop
+	 */
+	public String getInstalledOS() {
+		return this.installedOS;
+	}
+	
+	/**
+	 * Gets the fine per day for a late return.
+	 *
+	 * @return lateReturnFinePerDay The fine per day for a late return
+	 */
+	public double getLateReturnFinePerDay() {
+		return this.getLateReturnFinePerDay();
+	}
+	
+	/**
+	 * Gets the max fine amount.
+	 *
+	 * @return maxFineAmount The max fine amount
+	 */
+	public double getMaxFineAmount() {
+		return this.maxFineAmount;
+	}
+	
+	
 }
