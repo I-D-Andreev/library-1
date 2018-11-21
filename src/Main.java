2ee8a893926bbb06d103642a2229405847860ad1
Ivan <iivvaann.andreev@gmail.com>
2018-11-21 21:51:57 +0000

    Test address class in main.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //System.out.println("Hello world");
+        Address ad1 = new Address("Bay Campus, Fabian Way, Crymlyn Burrows", "Swansea", "UK", "SA1 8EP");
+        Address ad2 = new Address("Bay Campus, Fabian Way", "Crymlyn Burrows", "Swansea", "UK", "SA1 8EP");
+
+        System.out.println(ad1.toString());
+        System.out.println("--------");
+        System.out.println(ad2.toString());
     }
 }
