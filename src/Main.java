088261a0282fc09e294dd70817631ee70a5eaef1
Sian Pike <sian.pike@icloud.com>
2018-11-22 10:16:29 +0000

    When main is run - the login window now shows.  Added the initialisation method and moved the fxml files to a resource folder.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.Date;
 
-class Main {
+public class Main extends Application {
+
     public static void main(String[] args) {
         //System.out.println("Hello world");
         Address ad1 = new Address("Bay Campus, Fabian Way, Crymlyn Burrows", "Swansea", "UK", "SA1 8EP");
@@ -12,5 +20,30 @@ class Main {
 
         Date d = new Date();
         System.out.println(d);
+
+        launch(args);
+    }
+
+    /**
+     * Initialises and shows the login window.
+     *
+     * @param primaryStage The current window.
+     */
+    public void start(Stage primaryStage) {
+
+        Parent root = null;
+
+        try {
+
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
+
+        } catch (IOException e) {
+            //Changing later
+            e.printStackTrace();
+        }
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("TaweLib - Login");
+        primaryStage.show();
     }
 }
