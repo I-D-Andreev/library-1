2d5596520654cfd2a078fe51874fb1d092b2430f
Ivan <iivvaann.andreev@gmail.com>
2018-11-25 18:50:20 +0000

    Format Copy. Set due date. Constructor. Unique Id.

--- src/Copy.java
+++ src/Copy.java
@@ -1,31 +1,34 @@
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Copy {
 
-    //asdasojdashddasojdasojdoasjosaijd
+
     private String uniqueCopyID;
     private Resource copyOf;
+
     private User borrowedBy;
     private User reservedFor;
+
     private int loanDurationInDays;
-    //private Date borrowedOn;
-    //private Date dueDate;
+    private Date borrowedOn;
+    private Date dueDate;
+
     private History loanHistory;
     private static int nextId;
 
-    public Copy(String uniqueCopyID, Resource copyOf){
+    public Copy(int loanDurationInDays, Resource copyOf) {
 
-        this.uniqueCopyID=uniqueCopyID;
-        this.copyOf=copyOf;
+        this.copyOf = copyOf;
+        this.loanDurationInDays = loanDurationInDays;
+        this.loanHistory = new History();
+        this.setUniqueCopyID();
+
+        this.nullifyValues();
     }
 
     public String getUniqueCopyID() {
         return uniqueCopyID;
-    }
-
-    public void setUniqueCopyID(String uniqueCopyID) {
-        this.uniqueCopyID = uniqueCopyID;
     }
 
     public User getBorrowedBy() {
@@ -43,53 +46,79 @@ public class Copy {
     public void setLoanDurationInDays(int loanDurationInDays) {
         this.loanDurationInDays = loanDurationInDays;
     }
-    /*
+
+
     public Date getDueDate() {
         return dueDate;
     }
 
-    public void setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
+    public void setDueDate() {
+        // get the current date and add one day, to get the date tomorrow
+        Date tomorrowDate = new Date();
+        LocalDateTime.from(tomorrowDate.toInstant()).plusDays(1);
+
+        //add the loan duration to the date the copy was borrowed
+        // to get the other possible due date
+        Date dueDateLoan = new Date(borrowedOn.getTime());
+        LocalDateTime.from(dueDateLoan.toInstant()).plusDays(loanDurationInDays);
+
+        // The due date should be the bigger of the two
+        this.dueDate = (tomorrowDate.compareTo(dueDateLoan) == 1) ? tomorrowDate : dueDateLoan;
+
     }
-    */
+
     public History getLoanHistory() {
         return loanHistory;
     }
 
-    /*
-    public Date getBorrowedOn(){
+
+    public Date getBorrowedOn() {
         return this.borrowedOn;
     }
 
     public void setBorrowedOn(Date borrowedOn) {
         this.borrowedOn = borrowedOn;
     }
-    */
-    public User getReservedFor(){
+
+    public User getReservedFor() {
         return this.reservedFor;
     }
 
-    public void setReservedFor(User user){
-        this.reservedFor=user;
+    public void setReservedFor(User user) {
+        this.reservedFor = user;
     }
 
-    public static int getNextId() {
-        return nextId;
-    }
 
-    public boolean isAvailable(){
-        if (this.borrowedBy != null) {
-            return false;
-        }else return true;
-    }
-
-    /* todo Need Date class to be finished to do
-        else use API>?
-
-    public boolean shouldBeReturned(){
-
-        if (this.dueDate >= (this.borrowedOn +)){
+    public boolean isAvailable() {
+        if (this.borrowedBy == null && this.reservedFor == null) {
             return true;
-        }else return false;
-    }*/
+        } else {
+            return false;
+        }
+    }
+
+
+    public boolean shouldBeReturned() {
+        if (this.dueDate != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void nullifyValues() {
+        this.borrowedBy = null;
+        this.reservedFor = null;
+        this.borrowedOn = null;
+        this.dueDate = null;
+    }
+
+    public Resource getCopyOf() {
+        return copyOf;
+    }
+
+    private void setUniqueCopyID() {
+        this.uniqueCopyID = this.copyOf.getUniqueID() + nextId;
+        nextId++;
+    }
 }
