97f43c6a2e5c98a7b478c5de50daa82df7c45a9a
Ivan <iivvaann.andreev@gmail.com>
2018-11-28 18:57:08 +0000

    Auto format code lines.

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -2,122 +2,121 @@ import java.util.ArrayList;
 
 /**
  * The Class ResourceManager.
- * 
+ *
  * @author Steven Lewkowicz, Christina Meggs
  */
 
 public class ResourceManager {
 
-	private ArrayList<Resource> resources = new ArrayList<>();
+    private ArrayList<Resource> resources = new ArrayList<>();
 
-	public ResourceManager() {
-		selfPopulate();
-	}
-	
-	public void save() {
-		
-	}
-	
-	private void selfPopulate() {
-		
-	}
-	
-	public void addResource(Resource resource) {
-		this.resources.add(resource);
-	}
+    public ResourceManager() {
+        selfPopulate();
+    }
 
-	//thought resource manager manages resources not their copies
-	//dont get how this method works
-	public void addCopyOfResource(int loanDuration, Resource resource) {
-		
-	}
+    public void save() {
 
-	public void removeResource(Resource resource) {
-		this.resources.remove(resource);
-	}
-	
-	public void removeResource(String resourceId) {
-		if(this.getResourceById(resourceId)!=null){
-			this.removeResource(this.getResourceById(resourceId));
-		}
-	}
+    }
 
-	public void editResource(Resource resource, Resource newResource) {
-		this.resources.remove(resource);
-		this.resources.add(newResource);
-	}
-	
-	public Resource getResourceById(String resourceId) {
-		Resource r = null;
-		for (Resource resource : resources) {
-			if (resource.getUniqueID().equals(resourceId)) {
-				r = resource;
-			}
-		}
-		return r;
-	}
+    private void selfPopulate() {
 
-	public ArrayList<Resource> getAllResources() {
-		return resources;
-	
-	}
+    }
 
-	//Not sure how to implement
-	public ArrayList<Resource> getRequestedResourcesBy(User user) {
-		ArrayList<Resource> requested = new ArrayList<>();
+    public void addResource(Resource resource) {
+        this.resources.add(resource);
+    }
 
-		for (Resource resource : resources) {
-			ArrayList<Copy> copyList = resource.getCopyManager().getListOfAllCopies();
+    //thought resource manager manages resources not their copies
+    //dont get how this method works
+    public void addCopyOfResource(int loanDuration, Resource resource) {
 
-			for (Copy copy : copyList) {
-				if (copy.getReservedFor().equals(user)) {
-					requested.add(resource);
-				}
-			}
-		}
-		return requested;
-	}
+    }
 
-	/**
-	 * these not in copy manager instead of resource manager?
-	 public void removeCopy(Copy copy) {
-	 }
+    public void removeResource(Resource resource) {
+        this.resources.remove(resource);
+    }
 
-	 public void removeCopy(String copyId) {
-	 }
+    public void removeResource(String resourceId) {
+        if (this.getResourceById(resourceId) != null) {
+            this.removeResource(this.getResourceById(resourceId));
+        }
+    }
 
-	 public Copy getCopyById(String copyId) {
-	 return null;
-	 }
+    public void editResource(Resource resource, Resource newResource) {
+        this.resources.remove(resource);
+        this.resources.add(newResource);
+    }
 
-	 public void loanCopy(Resource reource, User toUser) {
+    public Resource getResourceById(String resourceId) {
+        Resource r = null;
+        for (Resource resource : resources) {
+            if (resource.getUniqueID().equals(resourceId)) {
+                r = resource;
+            }
+        }
+        return r;
+    }
 
-	 }
+    public ArrayList<Resource> getAllResources() {
+        return resources;
 
-	 public void reserveCopy(Resource resource, User forUser) {
+    }
 
-	 }
+    //Not sure how to implement
+    public ArrayList<Resource> getRequestedResourcesBy(User user) {
+        ArrayList<Resource> requested = new ArrayList<>();
 
-	 public void returnCopy(Copy copy, User user) {
+        for (Resource resource : resources) {
+            ArrayList<Copy> copyList = resource.getCopyManager().getListOfAllCopies();
 
-	 }
+            for (Copy copy : copyList) {
+                if (copy.getReservedFor().equals(user)) {
+                    requested.add(resource);
+                }
+            }
+        }
+        return requested;
+    }
 
-	 public ArrayList<Copy> getAllCopies() {
-	 return null;
-	 }
+    /**
+     * these not in copy manager instead of resource manager?
+     public void removeCopy(Copy copy) {
+     }
 
-	 public ArrayList<Copy> getReservedCopiesFor(User user) {
-	 return null;
-	 }
+     public void removeCopy(String copyId) {
+     }
 
-	 public ArrayList<Copy> getOverdueCopies() {
-	 return null;
-	 }
+     public Copy getCopyById(String copyId) {
+     return null;
+     }
 
-	 public ArrayList<Copy> getBorriedCopiesBy(User user) {
-	 return null;
-	 }
+     public void loanCopy(Resource reource, User toUser) {
 
-	 **/
-	
+     }
+
+     public void reserveCopy(Resource resource, User forUser) {
+
+     }
+
+     public void returnCopy(Copy copy, User user) {
+
+     }
+
+     public ArrayList<Copy> getAllCopies() {
+     return null;
+     }
+
+     public ArrayList<Copy> getReservedCopiesFor(User user) {
+     return null;
+     }
+
+     public ArrayList<Copy> getOverdueCopies() {
+     return null;
+     }
+
+     public ArrayList<Copy> getBorriedCopiesBy(User user) {
+     return null;
+     }
+     **/
+
 }
