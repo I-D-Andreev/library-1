e28db1a490f2d07e89e3b68c758f5912764e4c6f
Ivan <iivvaann.andreev@gmail.com>
2018-11-28 20:17:48 +0000

    Library class methods.

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,17 @@
 public class Library {
+    UserManager userManager;
+    ResourceManager resourceManager;
+
+    public Library() {
+        userManager = new UserManager();
+        resourceManager = new ResourceManager();
+    }
+
+    public UserManager getUserManager() {
+        return userManager;
+    }
+
+    public ResourceManager getResourceManager() {
+        return resourceManager;
+    }
 }
