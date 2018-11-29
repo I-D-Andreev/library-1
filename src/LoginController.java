f74a39fc58b198a67e36d7b8e332787f508bbd0a
Sian Pike <sian.pike@icloud.com>
2018-11-29 08:08:14 +0000

    Added so that it checks for username in list of users

--- src/LoginController.java
+++ src/LoginController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author Chris McAuley, Sian Pike
@@ -20,6 +21,10 @@ public class LoginController {
     private User user1 = new NormalUser("Sian", "Pike", "sianspike",
             "07813931066", "", new Address("14 Bryn Y Mor Crescent",
             "Swansea", "Wales", "SA14QT"));
+    private User user2 = new Librarian("Joe", "Bloggs", "librarian1",
+            "12345678", "", new Address("Somewhere", "Cardiff", "Wales",
+            "AB12CD"));
+    private ArrayList<User> userList = new ArrayList<User>();
 
     @FXML // fx:id="usernameTextField"
     private TextField usernameTextField; // Value injected by FXMLLoader
@@ -32,11 +37,13 @@ public class LoginController {
     private void loginButtonClicked(ActionEvent event) {
 
         Parent root;
+        userList.add(user1);
+        userList.add(user2);
 
-        if (usernameTextField.getText().toLowerCase().equals(user1.getUsername())) {
+        if (equals(usernameTextField.getText(), userList)) {
 
             try {
-
+                
                 root = FXMLLoader.load(getClass().getClassLoader().getResource("resources/UserDashboard.fxml"));
                 Stage stage = new Stage();
                 
@@ -56,5 +63,22 @@ public class LoginController {
                 System.exit(0);
             }
         }
+    }
+
+    private Boolean equals(String usernameText, ArrayList<User> userList) {
+
+        Boolean exists = false;
+        usernameText = usernameText.toLowerCase();
+
+        for (User user : userList) {
+
+            if (user.getUsername().toLowerCase().equals(usernameText)) {
+
+                exists = true;
+
+            }
+        }
+
+        return exists;
     }
 }
