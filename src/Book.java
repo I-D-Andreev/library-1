9973d841095f20484654f5a24be5c4e3e740f9a2
KleanthisLiontis <k_leanthis@hotmail.com>
2018-11-30 00:13:12 +0000

    Added comments to classes book and copy, copy still  has one uncommented method couldnt really tell what it does.
    Signed-off-by: KleanthisLiontis <k_leanthis@hotmail.com>

--- src/Book.java
+++ src/Book.java
@@ -1,14 +1,58 @@
-//TODO: Javadoc
+/**
+ *Class to model Book resources.
+ *
+ * @author Kleanthis Liontis
+ */
 public class Book extends Resource {
 
+    /**
+     * Author of the book.
+     */
     private String author;
+
+    /**
+     * Publisher of the book.
+     */
     private String publisher;
+
+    /**
+     * Genre of the book.
+     * This is optional.
+     */
     private String genre;
+
+    /**
+     * The International Standard Book Number.
+     * This is optional.
+     */
     private String ISBN;
+
+    /**
+     * The language of the book.
+     * This is optional.
+     */
     private String language;
+
+    /**
+     * The fine per day the user delays to return the book.
+     */
     private final double lateReturnFinePerDay;
+
+    /**
+     * The maximum amount you can be fined for a book.
+     */
     private final double maxFineAmount;
 
+    /**
+     * Constructs a book object.
+     *
+     * @param uniqueID The unique ID of the book.
+     * @param title The title of the book.
+     * @param year The year the book was published.
+     * @param thumbnail The thumbnail photo of the book.
+     * @param author The author of the book.
+     * @param publisher The publisher of the book.
+     */
     public Book(String uniqueID, String title, int year, String thumbnail, String author, String publisher) {
         super(uniqueID, title, year, thumbnail);
         this.author = author;
@@ -18,6 +62,19 @@ public class Book extends Resource {
         this.maxFineAmount = 25.00;
     }
 
+    /**
+     * Construct a book object including ISBN,genre and language.
+     *
+     * @param uniqueID The unique ID of the book.
+     * @param title The title of the book.
+     * @param year The year the book was published.
+     * @param thumbnail The thumbnail photo of the book.
+     * @param author The author of the book.
+     * @param publisher The publisher of the book.
+     * @param genre The genre of the book.
+     * @param ISBN The International Standard Book Number.
+     * @param language The language of the book.
+     */
     public Book(String uniqueID, String title, int year, String thumbnail, String author, String publisher,
                 String genre, String ISBN, String language) {
         // call the "smaller" constructor
@@ -27,42 +84,82 @@ public class Book extends Resource {
         this.language = language;
     }
 
+    /**
+     * Sets the author of the book.
+     * @param author The new author.
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    /**
+     * Gets the author of the book.
+     * @return author The author of the book.
+     */
     public String getAuthor() {
         return this.author;
     }
 
+    /**
+     * Sets the publisher of the book.
+     * @param publisher The new publisher of the book
+     */
     public void setPublisher(String publisher) {
         this.publisher = publisher;
     }
 
+    /**
+     * Gets the publisher of the book.
+     * @return publisher The publisher of the book.
+     */
     public String getPublisher() {
         return this.publisher;
     }
 
+    /**
+     * Sets the International Standard Book Number of the book.
+     * @param ISBN The new International Standard Book Number.
+     */
     public void setISBN(String ISBN) {
         this.ISBN = ISBN;
     }
 
+    /**
+     * Gets the International Standard Book Number of the book.
+     * @return ISBN The International Standard Book Number.
+     */
     public String getISBN() {
         return this.ISBN;
     }
 
+    /**
+     * Sets the new language of the book.
+     * @param language The new language of the book.
+     */
     public void setLanguage(String language) {
         this.language = language;
     }
 
+    /**
+     * Gets the language of the book.
+     * @return language The language of the book.
+     */
     public String getLanguage() {
         return this.language;
     }
 
+    /**
+     * Gets the late return fine per day of the book.
+     * @return lateReturnFinePerDay The late return fine per day of the book.
+     */
     public double getLateReturnFinePerDay() {
         return this.lateReturnFinePerDay;
     }
 
+    /**
+     * Gets the maximum fine a book can have.
+     * @return maxFineAmount The max fine of the book.
+     */
     public double getMaxFineAmount() {
         return this.maxFineAmount;
     }
--- src/Copy.java
+++ src/Copy.java
@@ -1,22 +1,67 @@
 import java.util.Date;
 
+/** Class to model a Copy of a resource.
+ *
+ * @author Kleanthis Liontis, Ivan Andreev.
+ */
 public class Copy {
 
-
+    /**
+     * The unique ID of each copy
+     */
     private String uniqueCopyID;
+
+    /**
+     * The resource this copy is a copy of.
+     */
     private Resource copyOf;
+
+    /**
+     * The copy manager of this copy.
+     */
     private CopyManager copysManager;
 
+    /**
+     * Who has borrowed this copy.
+     */
     private User borrowedBy;
+
+    /**
+     * Who is this copy reserved for.
+     */
     private User reservedFor;
 
+    /**
+     * The loan duration of this copy in days.
+     */
     private int loanDurationInDays;
+
+    /**
+     * When this copy was borrowed.
+     */
     private Date borrowedOn;
+
+    /**
+     * When this copy is expected to be returned to back to library.
+     */
     private Date dueDate;
 
+    /**
+     * The history on how many times this copy was loaned, when and by who.
+     */
     private History loanHistory;
+
+    /**
+     * The next ID of a copy.
+     */
     private static int nextId;
 
+    /**
+     * The constructor of a Copy.
+     *
+     * @param loanDurationInDays The loan duration in days of the copy.
+     * @param copysManager The copy manager of the copy.
+     */
     public Copy(int loanDurationInDays, CopyManager copysManager) {
 
         this.copysManager = copysManager;
@@ -24,35 +69,60 @@ public class Copy {
         this.loanDurationInDays = loanDurationInDays;
         this.loanHistory = new History();
         this.setUniqueCopyID();
-
         this.nullifyValues();
     }
 
+    /**
+     * Gets the unique ID of the copy.
+     * @return uniqueCopyID The unique copy of the copy.
+     */
     public String getUniqueCopyID() {
         return uniqueCopyID;
     }
 
+    /**
+     *Gets the user this copy was borrowed by.
+     * @return borrowedBy The user that borrowed the copy.
+     */
     public User getBorrowedBy() {
         return borrowedBy;
     }
 
+    /**
+     * Sets the user that borrowed this copy.
+     * @param borrowedBy The new user that borrowed the copy
+     */
     public void setBorrowedBy(User borrowedBy) {
         this.borrowedBy = borrowedBy;
     }
 
+    /**
+     * Gets the loan duration in days of the copy.
+     * @return loanDurationInDays The loan duration in days of the copy.
+     */
     public int getLoanDurationInDays() {
         return loanDurationInDays;
     }
 
+    /**
+     * Sets the loan duration in days of the copy.
+     * @param loanDurationInDays The new load duration in days of the copy.
+     */
     public void setLoanDurationInDays(int loanDurationInDays) {
         this.loanDurationInDays = loanDurationInDays;
     }
 
-
+    /**
+     * Gets the due date of the copy.
+     * @return dueDate The date the copy is due.
+     */
     public Date getDueDate() {
         return dueDate;
     }
 
+    /**
+     * Sets the due date for the Copy
+     */
     // TODO: test this method
     public void setDueDate() {
         // 3600 seconds in an hour, 24 hours a day, multiplied by 1000 to convert to milliseconds
@@ -72,37 +142,66 @@ public class Copy {
 
     }
 
+    /**
+     * Gets the loan history of the copy.
+     * @return loanHistory The history of loaning of the copy.
+     */
     public History getLoanHistory() {
         return loanHistory;
     }
 
-
+    /**
+     * Gets the date the copy was borrowed on.
+     * @return borrowedOn The date the copy was borrowed on.
+     */
     public Date getBorrowedOn() {
         return this.borrowedOn;
     }
 
+    /**
+     *Sets the day the copy was borrowed on.
+     * @param borrowedOn The new date the copy was borrowed on.
+     */
     public void setBorrowedOn(Date borrowedOn) {
         this.borrowedOn = borrowedOn;
     }
 
+    /**
+     * Gets the user the copy is reserved for.
+     * @return reservedFor The user this copy was reserved for.
+     */
     public User getReservedFor() {
         return this.reservedFor;
     }
 
+    /**
+     * Sets the user this copy is reserved for.
+     * @param user The new user this copy is reserved for.
+     */
     public void setReservedFor(User user) {
         this.reservedFor = user;
     }
 
-
+    /**
+     * Gets if the copy is available.
+     * @return True if the copy is not borrowed and not reserved for a user,
+     * False otherwise.
+     */
     public boolean isAvailable() {
         return this.borrowedBy == null && this.reservedFor == null;
     }
 
-
+    /**
+     * Gets if the book should be returned.
+     * @return True if the book has a due date,false otherwise.
+     */
     public boolean shouldBeReturned() {
         return this.dueDate != null;
     }
 
+    /**
+     * Method to reset a copy to null values after it is returned.
+     */
     private void nullifyValues() {
         this.borrowedBy = null;
         this.reservedFor = null;
@@ -110,11 +209,17 @@ public class Copy {
         this.dueDate = null;
     }
 
+    /**
+     * Gets the resource this copy is a copy of.
+     * @return copyOf The resource this copy is a copy of.
+     */
     public Resource getCopyOf() {
         return copyOf;
     }
 
-    // What happens when a copy has just been returned.
+    /**
+     * Method that goes through what happens when a copy has just been returned.
+     */
     public void returnCopy(){
         nullifyValues();
         NormalUser byUser = (NormalUser)this.borrowedBy;
@@ -137,6 +242,10 @@ public class Copy {
         return (today.compareTo(this.dueDate) == 1);
     }
 
+    /**
+     * Sets a fine to a user,also update copy's history with this new fined user
+     * @param user The user to be fined
+     */
     private void giveFineToUser(NormalUser user) {
         Date today = new Date();
         // 3600 seconds in an hour, 24 hours a day, multiplied by 1000 to convert to milliseconds
@@ -152,6 +261,10 @@ public class Copy {
                 (int)numberOfDaysOverdue, this));
     }
 
+    /**
+     * Sets the user this copy is loaned to,also update copy's history with this new loan.
+     * @param toUser User the copy is loaned to.
+     */
     public void loanCopyTo(NormalUser toUser){
         this.borrowedOn = new Date();
         this.borrowedBy = toUser;
@@ -159,11 +272,19 @@ public class Copy {
         toUser.getBorrowedCopies().add(this);
     }
 
+    /**
+     * Sets the uniqueCopyID of this copy.
+     */
     private void setUniqueCopyID() {
         this.uniqueCopyID = this.copyOf.getUniqueID() + nextId;
         nextId++;
     }
 
+    /**
+     * todo comment this method
+     * @param obj
+     * @return
+     */
     public boolean equals(Object obj) {
         if (obj == null) return false;
         if (obj == this) return true;
