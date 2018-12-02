852c8ead952740701bf3aeeea8697e76d448c8f9
Christina <952277@swansea.ac.uk>
2018-12-02 21:00:25 +0000

    Started implementing self population in ResourceManager, need add copies and sort history

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -1,10 +1,8 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.Scanner;
+import java.io.*;
 /**
  * The Class ResourceManager.
  *
@@ -27,22 +25,20 @@ public class ResourceManager {
         for (Resource r : resources) {
 
             printWriter.print(r.getUniqueID() + " " + r.getTitle() + " " + r.getYear() + " " + r.getThumbnailImagePath() + " ");
-            //printWriter.print(r.getCopies())
 
             if (r instanceof DVD) {
-                printWriter.print("DVD" + " " + ((DVD) r).getDirector() + " " + ((DVD) r).getRuntime() + " ");
+                printWriter.print("DVD" + " " + ((DVD) r).getDirector() + " " + ((DVD) r).getRuntime() + " " + ((DVD) r).getListOfSubtitleLanguages().size() + " ");
                 for (String lang : ((DVD) r).getListOfSubtitleLanguages()) {
                     printWriter.print(lang + " ");
                 }
             }
             else if (r instanceof Laptop) {
-                printWriter.print("Laptop" + " " + ((Laptop) r).getManufacturer() + " " + ((Laptop) r).getModel() + " " + ((Laptop) r).getInstalledOS());
+                printWriter.print("Laptop" + " " + ((Laptop) r).getManufacturer() + " " + ((Laptop) r).getModel() + " " + ((Laptop) r).getInstalledOS() + " ");
             }
             else if (r instanceof Book) {
                 printWriter.print("Book" + " " + ((Book) r).getAuthor() + " " + ((Book) r).getPublisher() + " "
-                        + ((Book) r).getGenre() + " " + ((Book) r).getISBN() + " " + ((Book) r).getLanguage());
+                        + ((Book) r).getGenre() + " " + ((Book) r).getISBN() + " " + ((Book) r).getLanguage() + " ");
             }
-            printWriter.print(r.getLateReturnFinePerDay() + " " + r.getMaxFineAmount() + " ");
 
             for (Copy c : r.getCopyManager().getListOfAllCopies()) {
                 //each copy has a copyManager, don't understand why this is there/how to write this to file
@@ -55,8 +51,61 @@ public class ResourceManager {
         printWriter.close();
     }
 
-    private void selfPopulate() {
+    private void selfPopulate() throws IllegalArgumentException {
+        File file = new File("Resources.txt");
+        Scanner in = null;
+        try {
+            in = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println ("Cannot open Resources.txt");
+            System.exit (0);
+        }
+        while (in.hasNextLine()) {
+            String subLine = in.nextLine();
+            Scanner line = new Scanner(subLine);
+            String uniqueID = line.next();
+            String title = line.next();
+            int year = line.nextInt();
+            String thumbnailImagePath = line.next();
+            String type = line.next();
 
+            if (type.equals("DVD")) {
+                String director = line.next();
+                int runtime = line.nextInt();
+                int listLength = line.nextInt();
+                ArrayList<String> listOfSubtitleLanguages = new ArrayList<>();
+                for (int i = 0; i<listLength;i++) {
+                    listOfSubtitleLanguages.add(line.next());
+                }
+                DVD r = new DVD(uniqueID, title, year, thumbnailImagePath, director, runtime);
+                r.setListOfSubtitleLanguages(listOfSubtitleLanguages);
+            } else if (type.equals("Laptop")) {
+                String manufacturer = line.next();
+                String model = line.next();
+                String installedOS = line.next();
+                Laptop r = new Laptop(uniqueID, title, year, thumbnailImagePath, manufacturer, model, installedOS);
+            } else if (type.equals("Book")) {
+                String author = line.next();
+                String publisher = line.next();
+                String genre = line.next();
+                String ISBN = line.next();
+                String language = line.next();
+                Book r = new Book(uniqueID, title, year, thumbnailImagePath, author, publisher, genre, ISBN, language);
+            }
+
+            ArrayList<Copy> copyManager = new ArrayList<>();
+            while (!line.next().equals("\n")) {
+                String uniqueCopyId = line.next();
+                String borrowedBy = line.next();
+                String reservedFor = line.next();
+                int loanDurationInDays = line.nextInt();
+                Date borrowedOn = new Date(in.nextLong());
+                Date dueDate = new Date(in.nextLong());
+
+                //history be sorted
+                //copy needs to be made
+            }
+        }
     }
 
     public void addResource(Resource resource) {
