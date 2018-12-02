1bfaf7c706e9faae728641d63b43ef446c5bc39e
Ivan <iivvaann.andreev@gmail.com>
2018-12-02 18:36:00 +0000

    Controller class to be super class of all controllers. Has the data that is passed around.

---
+++ src/Controller.java
@@ -0,0 +1,12 @@
+// Superclass of all controllers
+public class Controller {
+    private Library library;
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
+}
