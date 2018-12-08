8362b533a9c49d0d260564052891d1055e344c44
Ivan <iivvaann.andreev@gmail.com>
2018-12-08 17:40:08 +0000

    Overdue copies works

--- src/TableRepresentationCopyInformation.java
+++ src/TableRepresentationCopyInformation.java
@@ -1,10 +1,13 @@
 public class TableRepresentationCopyInformation {
 
     private String copyID;
+    private String borrowedBy;
     private String resourceName;
     private String resourceType;
     private String borrowDate;
     private String dueDate;
+    private Integer daysOverdue;
+
 
     public TableRepresentationCopyInformation(String copyID, String resourceName, String resourceType) {
         this.copyID = copyID;
@@ -17,6 +20,15 @@ public class TableRepresentationCopyInformation {
        this(copyID, resourceName, resourceType);
         this.borrowDate = borrowDate;
         this.dueDate = dueDate;
+    }
+
+    public TableRepresentationCopyInformation(String copyID, String borrowDate,
+                                              String dueDate, Integer daysOverdue, String borrowedBy) {
+        this.copyID = copyID;
+        this.borrowDate = borrowDate;
+        this.dueDate = dueDate;
+        this.daysOverdue = daysOverdue;
+        this.borrowedBy = borrowedBy;
     }
 
     public String getCopyID() {
@@ -37,5 +49,13 @@ public class TableRepresentationCopyInformation {
 
     public String getDueDate() {
         return dueDate;
+    }
+
+    public Integer getDaysOverdue() {
+        return daysOverdue;
+    }
+
+    public String getBorrowedBy() {
+        return borrowedBy;
     }
 }
