2b13f328a31a1bc1d9b70cf1d61a89d908bba2a4
Sian Pike <sian.pike@icloud.com>
2018-12-08 14:12:27 +0000

    Added reserved resources window for user dashboard

---
+++ src/ReservedResourcesController.java
@@ -0,0 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * Controller class for the reserved resources window.
+ * Handles the interaction between the user and UI.
+ *
+ * @author Sian Pike
+ */
+public class ReservedResourcesController extends Controller {
+
+    @FXML // fx:id="okButton"
+    private Button okButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="reservedResourcesTable"
+    private TableView<?> reservedResourcesTable; // Value injected by FXMLLoader
+
+    @FXML // fx:id="reservedResourcesColumn"
+    private TableColumn<?, ?> reservedResourcesColumn; // Value injected by FXMLLoader
+
+    @FXML
+    void okButtonClicked(ActionEvent event) {
+
+        new NewWindow("resources/UserDashboard.fxml", event, "Dashboard - TaweLib", getLibrary());
+    }
+
+}
